4a63ff73388552a789112cf1cec4fbc004196ce7
keneth cajas <kenocari@gmail.com>
2016-09-27 21:03:57 -0600

    aqui se actualizo todos los templete

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from blog.models import Post
 
 # Create your views here.
 
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	articulo = Post.objects.filter(published_date__lte=timezone.now())
+	return render(request, 'blog/post_list.html', {'articulo':articulo})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
